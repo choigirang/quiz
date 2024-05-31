3b02f2d46dfbf43e8e2bace051ce6cd8ed0c8e93
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-05-31 19:51:03 +0900

    feat: provider custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import WithProvider from './components/common/provider/WithProvider';
 import Header from './components/common/header/Header';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import QuizPage from './section/quiz/QuizPage';
 import AllPage from './section/all/AllPage';
 import ChartPage from './section/chart/ChartPage';
@@ -8,14 +8,14 @@ import ChartPage from './section/chart/ChartPage';
 function App() {
   return (
     <div className='flex w-full'>
-      <BrowserRouter>
+      <WithProvider>
         <Header />
         <Routes>
           <Route path='/' element={<QuizPage />}></Route>
           <Route path='/all' element={<AllPage />}></Route>
           <Route path='/chart' element={<ChartPage />}></Route>
         </Routes>
-      </BrowserRouter>
+      </WithProvider>
     </div>
   );
 }
