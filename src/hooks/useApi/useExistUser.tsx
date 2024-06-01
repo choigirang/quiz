73b15d178e28312e081fb5e6313f383c9dd67f71
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-01 20:19:44 +0900

    feat: get exist user data query hooks

--- src/hooks/useApi/useExistUser.tsx
+++ src/hooks/useApi/useExistUser.tsx
@@ -1,26 +1,48 @@
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../utils/api/firebase';
 import { useQuery } from 'react-query';
+import { useAppDispatch } from '../useRedux';
+import { login } from '../../store/modules/loginSlice';
+import { LoginData } from '../../type/login';
 
-export default function useExistUser(id: string, code: string) {
-  // check exist user
-  async function getExistUser(id: string, code: string) {
-    const docRef = doc(db, 'users', id);
-    const docSnap = await getDoc(docRef);
-    const user = docSnap.exists();
-    const userData = docSnap.data();
-
-    if (user && userData) {
-      if (userData.code === code) return alert('로그인 되었습니다.');
-      else return alert('일치하지 않는 사용자입니다.');
-    } else {
-      throw new Error('No such document!');
-    }
+// check exist user
+async function getExistUser(id: string, code: string) {
+  if (!id || !code) {
+    return Promise.reject(new Error('ID와 코드가 필요합니다.'));
   }
 
-  const queryResult = useQuery(['user', id], () => getExistUser(id, code), {
-    enabled: !!id, // id가 있을 때만 쿼리를 실행
-  });
+  const docRef = doc(db, 'users', id);
+  const docSnap = await getDoc(docRef);
+  const user = docSnap.exists();
+  const userData = docSnap.data();
 
-  return queryResult;
+  if (user && userData) {
+    if (userData.code === code) {
+      alert('로그인 되었습니다.');
+      return userData;
+    } else {
+      alert('일치하지 않는 사용자입니다.');
+      return Promise.reject(new Error('일치하지 않는 사용자입니다.'));
+    }
+  } else {
+    throw new Error('사용자를 찾을 수 없습니다.');
+  }
+}
+
+/** 24/06/01 - get exist user data query hooks */
+export default function useExistUser(id: string, code: string) {
+  const dispatch = useAppDispatch();
+
+  const { data, isLoading, isSuccess, refetch } = useQuery(
+    ['user', id],
+    () =>
+      getExistUser(id, code)
+        .then((res) => {
+          dispatch(login(res as LoginData));
+        })
+        .catch((err) => alert(err)),
+    { enabled: false }
+  );
+
+  return { data, isLoading, isSuccess, refetch };
 }
