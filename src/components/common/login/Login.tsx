cb04c50e6d9d4741400b7560530d2e9368e9f093
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-01 20:18:45 +0900

    feat: login func by new user or exist user

--- src/components/common/login/Login.tsx
+++ src/components/common/login/Login.tsx
@@ -1,29 +1,41 @@
-import React, { Suspense, useState } from 'react';
-import User from './User';
-import Loading from '../loading/Loading';
+import { useEffect, useState } from 'react';
 import UserInputs from './UserInputs';
+import useExistUser from '../../../hooks/useApi/useExistUser';
+import useSignUser from '../../../hooks/useApi/useSignUser';
+
+import SelectUser from './SelectUser';
 
 /** 24/05/30 - Login pop */
 export default function Login() {
-  const [user, setUser] = useState({ user: '', exist: false });
+  // (exist user && code set) || (new user && none code)
+  const [exist, setExist] = useState(false);
+  const [user, setUser] = useState({ id: '', code: '' });
 
-  const onSubmit = (exist: boolean) => {
-    if (exist) {
-      // data fetching(exist user) & set user name
-    } else {
-      // data post(new user) & set user name
+  // exist user data
+  const { refetch } = useExistUser(user.id, user.code);
+  // new user data
+  const { mutation } = useSignUser(user.id);
+
+  // get user data query
+  useEffect(() => {
+    if (exist && user.id && user.code) {
+      refetch();
     }
-  };
+  }, [user.code]);
+
+  // create new user query
+  useEffect(() => {
+    if (!exist && user.id) {
+      mutation.mutate();
+    }
+  }, [user.id]);
 
   return (
-    <React.Fragment>
-      {user.user ? (
-        <Suspense fallback={<Loading />}>
-          <User></User>
-        </Suspense>
-      ) : (
-        <UserInputs submit={onSubmit} />
-      )}
-    </React.Fragment>
+    <main className='flex flex-col gap-10 justify-center items-center w-full h-full'>
+      {/* func: change exist state */}
+      <SelectUser setExist={setExist} />
+      {/* func: sign or login */}
+      <UserInputs exist={exist} setUser={setUser} />
+    </main>
   );
 }
