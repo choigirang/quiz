f597fbba8acdf1872210772168000bd80bc7317e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-02 23:37:37 +0900

    feat: add stack quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import WithProvider from './components/common/provider/WithProvider';
 import Header from './components/common/header/Header';
 import { Route, Routes } from 'react-router-dom';
-import QuizPage from './section/quiz/QuizPage';
+import QuizPage from './section/quiz/home/QuizPage';
 import AllPage from './section/all/AllPage';
 import ChartPage from './section/chart/ChartPage';
+import Quiz from './section/quiz/stack/Quiz';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <WithProvider>
         <Header />
         <Routes>
-          <Route path='/' element={<QuizPage />}></Route>
+          <Route path='/quiz' element={<QuizPage />}></Route>
+          <Route path='/quiz/:stack' element={<Quiz />}></Route>
           <Route path='/all' element={<AllPage />}></Route>
           <Route path='/chart' element={<ChartPage />}></Route>
         </Routes>
