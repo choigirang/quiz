0faacbadbe56a1f9e63e9825fc22609a1da544e8
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-02 23:43:49 +0900

    create: quiz data slice

---
+++ src/store/modules/quizSlice.tsx
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { InitStack, QuizData } from '../../type/quiz';
+
+const initialState: QuizData[] = [];
+
+export const quizSlice = createSlice({
+  name: 'quizSlice',
+  initialState,
+  reducers: {
+    quiz: (state, action: PayloadAction<QuizData[]>) => {
+      return action.payload;
+    },
+    answer: (
+      state: QuizData[],
+      action: PayloadAction<{ num: number; answer: number }>
+    ) => {
+      const { num, answer } = action.payload;
+      const quizItem = state.find((quiz) => quiz.num === num);
+      if (quizItem) {
+        quizItem.state = quizItem.correct === answer;
+      }
+    },
+    resetQuiz: () => initialState,
+  },
+});
+
+export const { quiz, resetQuiz } = quizSlice.actions;
+export default quizSlice;
