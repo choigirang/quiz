c84ac0cf79d6153609bcd9620758baeb1a5d3628
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-02 23:37:46 +0900

    create example quiz data

---
+++ src/assets/quiz/js.ts
@@ -0,0 +1,13 @@
+import { QuizData } from '../../type/quiz';
+
+const jsQuiz: QuizData[] = [
+  {
+    num: 1,
+    title: 'What is TypeScript?',
+    Q: { 1: 'A programming language', 2: 'A type of food', 3: 'A car brand' },
+    state: false,
+    correct: 1,
+  },
+];
+
+export default jsQuiz;
