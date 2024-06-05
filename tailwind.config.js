64c8ae189ce5865f482dd0dec7e366ef868eb277
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-05 20:53:28 +0900

    add custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", ".src/*.{js,ts,jsx,tsx,mdx}"],
+  mode: 'jit',
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', '.src/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       transitionDelay: {
         default: 1,
+      },
+      gridTemplateColumns: {
+        stackCategory: 'repeat(5, auto)',
+      },
+      transitionProperty: {
+        default: 'all',
+      },
+      transitionTimingFunction: {
+        default: 'ease-in-out',
+      },
+      transitionDelay: {
+        default: '1s',
       },
     },
   },
