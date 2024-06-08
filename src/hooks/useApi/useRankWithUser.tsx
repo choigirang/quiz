4d4b7c12639b595edfb1d2841a8322ed597264b1
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-09 01:41:59 +0900

    feat: user score & rank by stack query hook

---
+++ src/hooks/useApi/useRankWithUser.tsx
@@ -0,0 +1,41 @@
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from '../../utils/api/firebase';
+import { useQuery } from 'react-query';
+
+async function getUserRank(id: string, stack: string | undefined) {
+  // !stack
+  if (!stack) return undefined;
+
+  const docRef = doc(db, 'rank', stack);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists()) {
+    return undefined;
+  }
+  const userData = docSnap.data();
+  const score = userData[id];
+
+  // !user data
+  if (score === undefined) {
+    return undefined;
+  }
+
+  // rank
+  const sortedRanks = Object.entries(userData).sort(
+    ([, scoreA], [, scoreB]) => (scoreB as number) - (scoreA as number)
+  );
+
+  const rank = sortedRanks.findIndex(([userId]) => userId === id) + 1;
+
+  return { score, rank };
+}
+
+export default function useRankWithUser(id: string, stack: string | undefined) {
+  const { data, isLoading, isSuccess, refetch } = useQuery(
+    ['rank', id],
+    () => getUserRank(id, stack),
+    { enabled: false }
+  );
+
+  return { data, refetch };
+}
