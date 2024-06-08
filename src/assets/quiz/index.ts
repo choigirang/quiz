1de9a94307f128f1f7275e5d45e675937e3df794
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-08 16:47:56 +0900

    create stack constant

---
+++ src/assets/quiz/index.ts
@@ -0,0 +1,12 @@
+import { CategoryData } from '../../type/quiz';
+
+/** 24/06/08 - category data */
+export const stackCategory: CategoryData = {
+  html: { name: 'Html', color: '#DC4A25' },
+  css: { name: 'Css', color: '#244BDD' },
+  js: { name: 'Javascript', color: '#EFD81A' },
+  ts: { name: 'Typescript', color: '#2F73BF' },
+  react: { name: 'React', color: '#5ED2F3' },
+  next: { name: 'Next', color: 'black' },
+  cs: { name: 'CS', color: 'black' },
+};
