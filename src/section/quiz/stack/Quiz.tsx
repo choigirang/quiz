20ffdd514b4278e916df5eb282a1879934c3081b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-08 16:52:16 +0900

    feat: selected quiz data setting
    - 첫 페이지 진입 시 데이터 무작위 섞기
    - 무작위로 섞은 데이터가 존재할 시 (유저 페이지 이동 등) 기존에 있던 문제로 데이터 세팅 (풀지 않은 문제만 활용)

--- src/section/quiz/stack/Quiz.tsx
+++ src/section/quiz/stack/Quiz.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
-import useCheckUser from '../../../hooks/useCheckUser';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux/useRedux';
+import useCheckUser from '../../../hooks/useCheckUser';
+
 import Question from './Question';
-import { useParams } from 'react-router-dom';
+
 import { QuizData } from '../../../type/quiz';
-import { quiz } from '../../../store/modules/quizSlice';
+import { quiz, resetQuiz } from '../../../store/modules/quizSlice';
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import { resetStack } from '../../../store/modules/quizStackSlice';
 
 /** 24/06/02 - random quiz page */
 export default function Quiz() {
@@ -15,16 +20,46 @@ export default function Quiz() {
   const { stack } = useParams();
   // redux
   const dispatch = useAppDispatch();
+  const alreadyQuiz = useAppSelector((state) => state.quizSlice);
+  // navi
+  const navi = useNavigate();
+  // reset quiz & selected stack
+  const reset = () => {
+    dispatch(resetStack());
+    dispatch(resetQuiz());
+    navi('/quiz');
+  };
+
+  const shuffle = (array: QuizData[]) => {
+    const shuffledArray = array.slice();
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[randomIndex]] = [
+        shuffledArray[randomIndex],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
 
   // setting redux data with selected data
   useEffect(() => {
     const settingData = async () => {
       try {
-        // dynamic import by selected stack
-        const quizModule = await import(`../../../assets/quiz/${stack}`);
-        const quizData: QuizData[] = quizModule.default;
-        setData(quizData);
-        dispatch(quiz(quizData));
+        // if already redux set quiz data
+        if (alreadyQuiz.length !== 0) {
+          setData(alreadyQuiz);
+        } else {
+          // dynamic import by selected stack
+          const quizModule = await import(`../../../assets/quiz/${stack}`);
+          const quizData: QuizData[] = Array.isArray(quizModule.default)
+            ? quizModule.default
+            : [quizModule.default];
+
+          const shuffledData = shuffle(quizData);
+          setData(shuffledData);
+          dispatch(quiz(quizData));
+        }
       } catch (error) {
         console.error('Failed to load quiz data:', error);
       }
@@ -33,15 +68,23 @@ export default function Quiz() {
     if (stack) {
       settingData();
     }
-  }, []);
+  }, [stack]);
 
   return (
     <section className='w-full flex justify-center items-center'>
       {data ? (
-        <Question allQuiz={data} />
+        <Question data={data} setData={setData} />
       ) : (
         <div>서버 오류로 불러온 데이터가 없습니다.</div>
       )}
+      {/* reset quiz */}
+      <button
+        type='button'
+        onClick={reset}
+        className='absolute w-6 h-6 right-5 bottom-5 transition-default hover:animate-spin hover:text-yellow-400'
+      >
+        <ArrowPathIcon />
+      </button>
     </section>
   );
 }
