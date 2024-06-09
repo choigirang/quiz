60087f7344759f3c4a3948be712da93d61485899
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-10 00:03:29 +0900

    add custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,15 @@ module.exports = {
       transitionDelay: {
         default: '1s',
       },
+      colors: {
+        defaultYellow: '#F4CE14',
+        defaultGreen: '#495E57',
+        defaultBlack: '#45474B',
+        defaultWhite: '#F5F7F8',
+      },
+      textColor: {
+        default: '#45474B', // 기본 텍스트 색상을 defaultBlack으로 지정합니다.
+      },
     },
   },
   plugins: [],
