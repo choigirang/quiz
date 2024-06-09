26dab0f463f78244199e9cf383b22bf08a13b454
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-09 23:49:51 +0900

    feat: add children for hoc & move folder

--- src/components/common/quiz/Enter.tsx
+++ src/components/common/quiz/Enter.tsx
@@ -1,15 +1,15 @@
 import React, { SetStateAction, useState } from 'react';
-import { CategoryData, SelectQuizCategory } from '../../../type/quiz';
-import { CategroyState } from './QuizPage';
-import { useAppSelector } from '../../../hooks/redux/useRedux';
+import { CategroyState, SelectQuizCategory } from '../../../type/quiz';
 import { stackCategory } from '../../../assets/quiz';
 
 type EnterProps = {
+  children: JSX.Element | null;
+  category: string | null;
   setCategory: React.Dispatch<SetStateAction<CategroyState>>;
 };
 
 /** 24/06/02 - select stack category */
-export default function Enter({ setCategory }: EnterProps) {
+export default function Enter({ children, category, setCategory }: EnterProps) {
   const [selected, setSelected] = useState<SelectQuizCategory | null>();
 
   //   select category stack
@@ -21,8 +21,12 @@ export default function Enter({ setCategory }: EnterProps) {
   return (
     <section className='relative'>
       <div className='flex flex-col justify-center items-center mb-5'>
-        <h2 className='text-xl font-black'>환영합니다.</h2>
-        <h2>카테고리를 선택해주세요.</h2>
+        {children}
+        {category ? (
+          <h2>{`${stackCategory[category].name}를 선택하셨습니다.`}</h2>
+        ) : (
+          <h2>카테고리를 선택해주세요.</h2>
+        )}
       </div>
       {/* list */}
       <ul className={`${!selected ? 'grid grid-cols-5 gap-3' : 'none-style'}`}>
@@ -33,19 +37,13 @@ export default function Enter({ setCategory }: EnterProps) {
           return (
             <li
               key={key}
-              className='border-2 py-5 px-3 text-center rounded font-black cursor-pointer shadow-md transition-all duration-300 ease-in-out'
+              className='border-2 py-5 px-3 text-center rounded font-black cursor-pointer shadow-md transition-all hover:scale-110 duration-300 ease-in-out'
               //   select category change style
               style={{
                 display: !selected ? 'block' : isSelected ? 'block' : 'none',
-                borderColor: stackCategory[key].color,
                 color: stackCategory[key].color,
                 backgroundColor: 'white',
                 opacity: !selected ? 100 : isSelected ? 100 : 0,
-                transform: !selected
-                  ? 'scale(1)'
-                  : isSelected
-                    ? 'scale(1.5)'
-                    : 'scale(0)',
               }}
               onClick={() => handleClick(key)}
             >
