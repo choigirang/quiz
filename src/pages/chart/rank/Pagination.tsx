0e7e89b5cd986fff6aa453e4044c75090f1adea7
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-10 00:01:57 +0900

    feat: pagination with react-paginate

---
+++ src/pages/chart/rank/Pagination.tsx
@@ -0,0 +1,30 @@
+import React, { SetStateAction, useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+
+type PageProps = {
+  rankLeng: number;
+  setPage: React.Dispatch<SetStateAction<number>>;
+};
+
+/** 24/06/09 - ranking pagination */
+export default function Pagination({ rankLeng, setPage }: PageProps) {
+  const pagePerTotal = Math.ceil(rankLeng / 20);
+  const pageList = Array.from(Array(pagePerTotal), (_, idx) => idx + 1);
+
+  return (
+    <ReactPaginate
+      containerClassName='flex gap-3'
+      pageClassName='bg-gray-100'
+      pageLinkClassName='py-1 px-2 rounded'
+      activeLinkClassName='text-white font-bold bg-yellow-400 rounded'
+      pageRangeDisplayed={5}
+      previousLabel={null}
+      nextLabel={null}
+      onPageChange={(e) => {
+        setPage(e.selected + 1);
+      }}
+      breakLabel='...'
+      pageCount={pageList.length}
+    />
+  );
+}
