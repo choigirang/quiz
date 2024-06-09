b55acc6c809cfacbadbd5e139e9209d2aae1ed3b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-09 23:50:51 +0900

    create graph query hook for Graph page

---
+++ src/hooks/useApi/useGraph.tsx
@@ -0,0 +1,21 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { useQuery } from 'react-query';
+import { db } from '../../utils/api/firebase';
+
+async function getGraph(stack: string) {
+  const docRef = doc(db, 'rank', stack);
+  const stackRef = (await getDoc(docRef)).data();
+
+  return stackRef;
+}
+
+/** 24/06/09 - get all user data by stack */
+export default function useGraph(stack: string) {
+  const { data, isLoading, refetch } = useQuery(
+    ['graph'],
+    () => getGraph(stack),
+    { enabled: false }
+  );
+
+  return { data, isLoading, refetch };
+}
