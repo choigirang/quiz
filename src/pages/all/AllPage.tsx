0c9cd0bd7234e956557133627c9bac3f928039e4
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-10 19:25:06 +0900

    feat: divide route & add reset sheet stack btn

--- src/pages/all/AllPage.tsx
+++ src/pages/all/AllPage.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from 'hooks/redux/useRedux';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useAppDispatch } from 'hooks/redux/useRedux';
 
-import Enter from '../../components/common/quiz/Enter';
-import Data from './Data';
+import Enter from '../../components/quiz/Enter';
+import Data from './aboutData/Data';
+import ResetBtn from 'components/common/ResetBtn';
 
 import { selectSheet } from 'store/modules/sheetStackSlice';
 
 import { CategroyState } from '../../type/quiz';
-import { Route, Routes, useNavigate } from 'react-router-dom';
 
 /** 24/06/09 - all Sheet about stack */
 export default function AllPage() {
@@ -19,42 +20,38 @@ export default function AllPage() {
   const navi = useNavigate();
 
   const dispatch = useAppDispatch();
-  const sheetStack = useAppSelector((state) => state.sheetStackSlice);
 
   useEffect(() => {
-    if (sheetStack.stack !== '') {
-      setCategory({ stack: sheetStack.stack, display: false });
-      navi(`/all/${sheetStack.stack}`);
-    }
-
     if (category.stack) {
       // display timer with change page
       const timer = setTimeout(() => {
         setCategory((prev) => ({ ...prev, display: false }));
+        navi(`/all/${category.stack}`);
       }, 1000);
       // set global state stack
       dispatch(selectSheet(category.stack));
-      navi(`/all/${category.stack}`);
 
       return () => clearTimeout(timer);
     }
   }, [category.stack]);
 
   return (
-    <section className='flex flex-col justify-center items-center w-full'>
-      {!sheetStack && category.display && (
-        <Enter category={category.stack} setCategory={setCategory}>
-          <h2 className='text-xl font-black'>
-            모아보기 할 데이터를 가져옵니다.
-          </h2>
-        </Enter>
-      )}
+    <section className='relative flex flex-col justify-center items-center w-full'>
       <Routes>
         <Route
-          path={`/${category.stack}`}
-          element={<Data stack={category.stack} />}
-        />
+          path='/'
+          element={
+            category.display && (
+              <Enter category={category.stack} setCategory={setCategory}>
+                <h2 className='text-xl font-black'>모아보기 할 데이터를 가져옵니다.</h2>
+              </Enter>
+            )
+          }
+        ></Route>
+        <Route path={`/:stack`} element={<Data stack={category.stack} />} />
       </Routes>
+      {/* reset stack */}
+      <ResetBtn name='all' setDisplay={setCategory} />
     </section>
   );
 }
