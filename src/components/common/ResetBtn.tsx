9c09af65d2fea8dd3fa352d8fdd5402ff0fe27d3
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-10 19:22:39 +0900

    feat: divide reset btn

---
+++ src/components/common/ResetBtn.tsx
@@ -0,0 +1,60 @@
+import React, { SetStateAction, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from 'hooks/redux/useRedux';
+
+import { resetQuiz } from 'store/modules/quizSlice';
+import { resetStack } from 'store/modules/quizStackSlice';
+import { resetSheet } from 'store/modules/sheetStackSlice';
+
+import { CategroyState } from 'type/quiz';
+
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+
+type ResetProps = {
+  name: string;
+  setDisplay?: React.Dispatch<SetStateAction<CategroyState>>;
+};
+
+/** 24/06/10 - reset btn */
+export default function ResetBtn({ name, setDisplay }: ResetProps) {
+  // reset tooltip
+  const [tooltip, setTooltip] = useState(false);
+  // navi
+  const navi = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const { stack } = useAppSelector((state) => state.selectStackSlice);
+  const { stack: sheet } = useAppSelector((state) => state.sheetStackSlice);
+
+  const reset = () => {
+    if (name === 'quiz' && stack) {
+      dispatch(resetStack());
+      dispatch(resetQuiz());
+      navi(`/quiz`);
+    } else if (name === 'all' && sheet) {
+      dispatch(resetSheet());
+      if (setDisplay) setDisplay({ stack: '', display: true });
+      navi(`/all`);
+    }
+  };
+
+  return (
+    <div
+      onClick={reset}
+      onMouseEnter={() => setTooltip(true)}
+      onMouseLeave={() => setTooltip(false)}
+      className='absolute flex justify-center items-center p-1 right-5 bottom-5 bg-defaultYellow rounded-lg cursor-pointer'
+    >
+      <div
+        className={`absolute left-1/2 -translate-x-1/2 whitespace-nowrap bg-defaultYellow text-defaultGreen px-2 py-1 transition-default rounded ${tooltip ? 'opacity-100 -top-8' : 'opacity-0 -top-4'} -z-10`}
+      >
+        초기화
+      </div>
+      <ArrowPathIcon
+        width={24}
+        height={24}
+        className={`text-defaultGreen ${tooltip && 'animate-spin'}`}
+      />
+    </div>
+  );
+}
