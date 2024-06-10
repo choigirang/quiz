46c1636d09deb790e84670803be83ba87e544c79
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-10 19:18:28 +0900

    feat: divide login user info

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,30 +1,28 @@
 import { Link } from 'react-router-dom';
-import Category from './Category';
 import { useAppSelector } from '../../../hooks/redux/useRedux';
 
-import { UserIcon } from '@heroicons/react/24/outline';
+import Category from './Category';
+
+import User from './User';
 
 /** 2024/05/30 - header */
 export default function Header() {
   const user = useAppSelector((state) => state.loginSlice);
 
   return (
-    <header className='relative w-[150px] h-screen p-5 border-r'>
-      {/* logo */}
-      <h1 className='font-black'>
-        <Link to={'/quiz'}>
-          DEV<br></br>Quiz
-        </Link>
-      </h1>
-      {/* category */}
-      <Category />
+    <header className='flex flex-col justify-between w-[150px] h-screen p-5 border-r'>
+      <div>
+        {/* logo */}
+        <h1 className='font-black'>
+          <Link to={'/quiz'}>
+            DEV<br></br>Quiz
+          </Link>
+        </h1>
+        {/* category */}
+        <Category />
+      </div>
       {/* user login */}
-      {user.id && (
-        <div className='absolute left-1/2 bottom-5 -translate-x-1/2 flex items-center gap-2'>
-          <UserIcon width={16} height={16} />
-          <span>{user.id}</span>
-        </div>
-      )}
+      {user.id && <User id={user.id} />}
     </header>
   );
 }
