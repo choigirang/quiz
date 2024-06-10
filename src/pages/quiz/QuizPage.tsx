735a420d21453c3ff0a62279a83301252b241214
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-10 19:26:56 +0900

    feat: divide reset btn

--- src/pages/quiz/QuizPage.tsx
+++ src/pages/quiz/QuizPage.tsx
@@ -1,22 +1,18 @@
 import { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-
+import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'hooks/redux/useRedux';
 import useCheckUser from 'hooks/useCheckUser';
 
 import Question from './aboutQuiz/Question';
+import ResetBtn from 'components/common/ResetBtn';
 
 import { QuizData } from 'type/quiz';
-import { resetStack } from 'store/modules/quizStackSlice';
 
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import { quiz, resetQuiz } from 'store/modules/quizSlice';
+import { quiz } from 'store/modules/quizSlice';
 
 /** 24/06/02 - random quiz page */
 export default function QuizPage() {
   const [data, setData] = useState<QuizData[]>();
-  // reset tooltip
-  const [tooltip, setTooltip] = useState(false);
   // move page when user direct page
   const {} = useCheckUser('/quiz');
   // selected stack
@@ -24,14 +20,7 @@ export default function QuizPage() {
   // redux
   const dispatch = useAppDispatch();
   const alreadyQuiz = useAppSelector((state) => state.quizSlice);
-  // navi
-  const navi = useNavigate();
   // reset quiz & selected stack
-  const reset = () => {
-    dispatch(resetStack());
-    dispatch(resetQuiz());
-    navi('/quiz');
-  };
 
   const shuffle = (array: QuizData[]) => {
     const shuffledArray = array.slice();
@@ -81,22 +70,7 @@ export default function QuizPage() {
         <div>서버 오류로 불러온 데이터가 없습니다.</div>
       )}
       {/* reset quiz */}
-      <div className='absolute right-5 bottom-5'>
-        <div
-          className={`absolute left-1/2 -translate-x-1/2 whitespace-nowrap bg-defaultYellow text-white px-2 py-1 transition-default rounded ${tooltip ? 'opacity-100 -top-8' : 'opacity-0 -top-4'}`}
-        >
-          초기화
-        </div>
-        <button
-          type='button'
-          onClick={reset}
-          onMouseEnter={() => setTooltip(true)}
-          onMouseLeave={() => setTooltip(false)}
-          className='w-6 h-6 transition-default'
-        >
-          <ArrowPathIcon className='hover:animate-spin hover:text-yellow-400' />
-        </button>
-      </div>
+      <ResetBtn name='quiz' />
     </section>
   );
 }
