b846330b3431e5c2d607704545f0ab63560fda84
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-11 10:56:30 +0900

    add font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/style/index.css';
+import '../src/style/font.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
