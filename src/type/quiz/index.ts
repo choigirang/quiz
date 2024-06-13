aaaea7194f68599d2109bd3d48613611a631b8a5
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-13 23:31:26 +0900

    feat: edit category name type

--- src/type/quiz/index.ts
+++ src/type/quiz/index.ts
@@ -1,20 +1,12 @@
 import { Color } from 'chart.js';
 
 /** 24/06/01 - quiz category data */
-export type SelectQuizCategory =
-  | 'html'
-  | 'css'
-  | 'js'
-  | 'ts'
-  | 'react'
-  | 'next'
-  | 'cs'
-  | string;
+export type SelectQuizCategory = 'html' | 'css' | 'js' | 'ts' | 'react' | 'next' | 'cs' | string;
 
 /** 24/06/01 - quiz category in Enter */
 export type CategoryData = {
   [key in SelectQuizCategory]: {
-    name: string | null;
+    name: string;
     color: string;
   };
 };
