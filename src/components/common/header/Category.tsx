c965500bf047df8f8182a0053e72b64eaed60540
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-06-13 17:26:33 +0900

    optimization use memo & use call back

--- src/components/common/header/Category.tsx
+++ src/components/common/header/Category.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { useAppSelector } from '../../../hooks/redux/useRedux';
@@ -16,22 +17,25 @@ export default function Category() {
   const { stack } = useAppSelector((state) => state.selectStackSlice);
   const { stack: sheet } = useAppSelector((state) => state.sheetStackSlice);
 
-  const createLink = (link: string) => {
-    if (link === '/quiz') {
-      if (stack) return `/quiz/${stack}`;
-      else return '/quiz';
-    }
+  const categoryEntries = useMemo(() => Object.entries(CATEGORY), []);
 
-    if (link === '/all') {
-      if (sheet) return `/all/${sheet}`;
-      else return '/all';
-    } else return link;
-  };
+  const createLink = useMemo(
+    () => (link: string) => {
+      if (link === '/quiz') {
+        return stack ? `/quiz/${stack}` : '/quiz';
+      }
+      if (link === '/all') {
+        return sheet ? `/all/${sheet}` : '/all';
+      }
+      return link;
+    },
+    [stack, sheet]
+  );
 
   return (
     <ul className='flex flex-col mt-10 gap-10 text-sm'>
       {/* category mapping */}
-      {Object.entries(CATEGORY).map(([key, value]) => {
+      {categoryEntries.map(([key, value]) => {
         // if login & selected stack && home page = seleted stack page
         return (
           <li key={key}>
